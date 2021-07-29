7a890d4fd117993bdd8c5820e9f1100d0966402e
Robert Lehmann <lehmrob@gmail.com>
2021-07-29 22:32:06 +0200

    Update dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,4 +3,13 @@ if status is-interactive
 end
 
 source ~/.alias
-source ~/.exports
+
+# exports
+
+set -x EDITOR nvim
+set -x DOT $HOME/dotfiles
+set -x GOROOT /usr/local/go
+set -x GOPATH $HOME/Work/go
+set -x GOPRIVATE "172.16.40.5"
+set -x PATH $PATH:$GOROOT/bin:$GOPATH/bin:$HOME/bin:$HOME/usr/bin:$POETRYPATH/bin
+set -x PATH $PATH:$HOME/.local/bin
