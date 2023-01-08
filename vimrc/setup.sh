beb984aa3c62b5883848358f938bc33d5ccfe34d
Robert Lehmann <robert.lehmann@sitec-systems.de>
2023-01-08 20:49:33 +0100

    Update dotfiles

--- vimrc/setup.sh
+++ vimrc/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 ln -sf "$PWD/.vimrc" "$HOME/.vimrc"
+
+if [ ! -d "$HOME/.vim/autoload" ]; then
+    mkdir -p "$HOME/.vim/autoload"
+fi
+
+ln -sf "$PWD/autoload/plug.vim" "$HOME/.vim/autoload/plug.vim"
